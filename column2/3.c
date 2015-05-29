e9c8442c3b6ddcae9c73f3addcd891117372cc33
Emanouil Matsis <emmanouilmatsis@gmail.com>
2015-05-29 19:38:25 -0400

    modified:   3.c

--- column2/3.c
+++ column2/3.c
@@ -5,12 +5,10 @@
 
 //#define JUGGLE_OUR_SOLUTION
 //#define JUGGLE_OUR_SOLUTION
-#define REVERSE
+#define RECURSIVE
+//#define REVERSE
 
 
-int gcd(int, int);
-void reverse(char* arr, int, int);
-void swap(char*, int, int);
 char *rotate(char*, int, int);
 
 
@@ -24,42 +22,8 @@ int main(int argc, char** argv)
 }
 
 
-int gcd(int i, int j)
-{
-    while (i != j)
-        if (i > j)
-            i -= j;
-        else
-            j -= i;
-
-    return i;
-}
-
-void reverse(char *arr, int start, int end)
-{
-    int i = start;
-    int j = end;
-
-    if (start >= end)
-        return;
-
-    while (i < j)
-        swap(arr, i++, j--);
-}
-
-void swap(char *arr, int i, int j)
-{
-    if (i == j)
-        return;
-
-    char t = arr[i];
-    arr[i] = arr[j];
-    arr[j] = t;
-}
-
-
-char *rotate(char* x, int n, int rotdist)
 #ifdef JUGGLE_OUR_SOLUTION
+char *rotate(char* x, int n, int rotdist)
 {
     if (!rotdist)
         return;
@@ -84,6 +48,18 @@ char *rotate(char* x, int n, int rotdist)
 #endif
 
 #ifdef JUGGLE_BOOK_SOLUTION
+int gcd(int i, int j)
+{
+    while (i != j)
+        if (i > j)
+            i -= j;
+        else
+            j -= i;
+
+    return i;
+}
+
+char *rotate(char* x, int n, int rotdist)
 {
     for (int i = 0; i < gcd(rotdist, n); i++)
     {
@@ -107,7 +83,79 @@ char *rotate(char* x, int n, int rotdist)
 }
 #endif
 
+#ifdef RECURSIVE
+void swap(char *arr, int start_a, int start_b, int length)
+{
+    char t;
+
+    for (int i = 0; i < length; i++)
+    {
+        t = arr[start_a + i];
+        arr[start_a + i] = arr[start_b + i];
+        arr[start_b + i] = t;
+    }
+}
+
+void recursive_swap(char *arr, int n, int start, int size_a)
+{
+    printf("n=%i start=%i size_a=%i\n", n, start, size_a);
+    for (int i = start; i <= start + n - 1; i++)
+        printf("%c", arr[i]);
+    printf("\n");
+
+    if (size_a <= n - size_a)
+        swap(arr, start, n - size_a + start, size_a);
+    else
+        swap(arr, start, size_a + start, n - size_a);
+
+    for (int i = start; i <= start + n - 1; i++)
+        printf("%c", arr[i]);
+    printf("\n");
+
+    if (n != 2 * size_a)
+    {
+        if (size_a <= n - size_a)
+            recursive_swap(arr, n - size_a, start, size_a);
+        else
+            recursive_swap(arr, size_a, start + n - size_a, 2 * size_a - n);
+    }
+}
+
+char *rotate(char* x, int n, int rotdist)
+{
+    if (rotdist == 0 || rotdist == n)
+        return x;
+
+    recursive_swap(x, n, 0, ((rotdist % n) + n) % n);
+
+    return x;
+}
+#endif
+
 #ifdef REVERSE
+void swap(char *arr, int i, int j)
+{
+    if (i == j)
+        return;
+
+    char t = arr[i];
+    arr[i] = arr[j];
+    arr[j] = t;
+}
+
+void reverse(char *arr, int start, int end)
+{
+    int i = start;
+    int j = end;
+
+    if (start >= end)
+        return;
+
+    while (i < j)
+        swap(arr, i++, j--);
+}
+
+char *rotate(char* x, int n, int rotdist)
 {
     reverse(x, 0, rotdist-1);
     reverse(x, rotdist, n-1);
