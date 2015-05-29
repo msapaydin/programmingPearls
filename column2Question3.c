5969e007efdc50fe895e8c9a64fcaf027209a4b2
Emanouil Matsis <emmanouilmatsis@gmail.com>
2015-05-29 17:00:26 -0400

    modified:   column2Question3.c

--- column2Question3.c
+++ column2Question3.c
@@ -3,23 +3,63 @@
 #include <string.h>
 
 
+//#define JUGGLE_OUR_SOLUTION
+//#define JUGGLE_OUR_SOLUTION
+#define REVERSE
+
+
 int gcd(int, int);
-void rotate(char*, int, int);
+void reverse(char* arr, int, int);
+void swap(char*, int, int);
+char *rotate(char*, int, int);
 
 
 int main(int argc, char** argv)
 {
     char arr[] = "abcdefgh";
 
-    rotate(arr, strlen(arr), atoi(argv[1]));
-
-    printf("%s\n", arr);
+    printf("%s\n", rotate(arr, strlen(arr), atoi(argv[1])));
 
     return 0;
 }
 
 
-void rotate(char* x, int n, int rotdist)
+int gcd(int i, int j)
+{
+    while (i != j)
+        if (i > j)
+            i -= j;
+        else
+            j -= i;
+
+    return i;
+}
+
+void reverse(char *arr, int start, int end)
+{
+    int i = start;
+    int j = end;
+
+    if (start >= end)
+        return;
+
+    while (i < j)
+        swap(arr, i++, j--);
+}
+
+void swap(char *arr, int i, int j)
+{
+    if (i == j)
+        return;
+
+    char t = arr[i];
+    arr[i] = arr[j];
+    arr[j] = t;
+}
+
+
+char *rotate(char* x, int n, int rotdist)
+#ifdef JUGGLE_OUR_SOLUTION
 {
     if (!rotdist)
         return;
@@ -41,3 +81,38 @@ void rotate(char* x, int n, int rotdist)
 
     x[n - rotdist + d] = t;
 }
+#endif
+
+#ifdef JUGGLE_BOOK_SOLUTION
+{
+    for (int i = 0; i < gcd(rotdist, n); i++)
+    {
+        char t = x[i];
+        int j = i;
+
+        while (1)
+        {
+            int k = j + rotdist;
+            if (k >= n)
+                k -= n;
+            if (k == i)
+                break;
+            x[j] = x[k];
+            j = k;
+        }
+        x[j] = t;
+    }
+
+    return x;
+}
+#endif
+
+#ifdef REVERSE
+{
+    reverse(x, 0, rotdist-1);
+    reverse(x, rotdist, n-1);
+    reverse(x, 0, n-1);
+
+    return x;
+}
+#endif
